0ce6825c876e7e37c122effd8d8ababb9469c76e
pedro_igor <pedrode13souza@gmail.com>
2024-01-26 17:36:04 -0300

    Change Button Sair

--- Assets/Scripts/LabScripts/MoveToLab.cs
+++ Assets/Scripts/LabScripts/MoveToLab.cs
@@ -8,71 +8,38 @@ public class MoveToLab : MonoBehaviour
     [SerializeField] Vector3 posicao2junto;
     [SerializeField] Vector3 posicao2separado;
 
-    public GameObject load1;
-    public GameObject load2;
+    
+    private bool first = true;
 
-   
-    /*public void Teleporte()
+    public void LoadScene()
     {
-        GameObject XR = GameObject.FindGameObjectWithTag("Player");
-        ActiveAvatar activeAvatar = XR.GetComponent<ActiveAvatar>();
-
-        if (SetGameConfig.JUNTO)
-        {
-            if (activeAvatar.player.Equals("Player1"))
-            {
-                XR.transform.position = posicao1;
-            }
-            else if (activeAvatar.player.Equals("Player2"))
-            {
-                XR.transform.position = posicao2;
-            }
-        }
-        else
-        {
-            if (activeAvatar.player.Equals("Player1"))
-            {
-                XR.transform.position = posicao1;
-            }
-            else if(activeAvatar.player.Equals("Player2"))
-            {
-                XR.transform.position = posicao3;
-            }
-        }
-    }*/
-
-    public void LoadScene1()
-    {
-        StartCoroutine(aoPressionar1());
-    }
-    public void LoadScene2()
-    {
-        StartCoroutine(aoPressionar2());
+        StartCoroutine(aoPressionar());
     }
 
-    IEnumerator aoPressionar2()
+    IEnumerator aoPressionar()
     {
         yield return new WaitForSeconds(2);
         GameObject XR = GameObject.FindGameObjectWithTag("Player");
-        ActiveAvatar activeAvatar = XR.GetComponent<ActiveAvatar>();
-        if (SetGameConfig.JUNTO)
-        {
-            XR.transform.position = posicao2junto;
+        
+        if ( first)
+        {           
+            XR.transform.position = posicao1;
+            first = false;
+            TimeCounter time = GameObject.FindObjectOfType<TimeCounter>();
+            time.StartCounter();
         }
         else
-        {
-            XR.transform.position = posicao2separado;
-        }
-        load2.SetActive(false);
-
+        {            
+            if (SetGameConfig.JUNTO)
+            {
+                XR.transform.position = posicao2junto;
+            }
+            else
+            {
+                XR.transform.position = posicao2separado;
+            }
+            
+        }       
     }
 
-    IEnumerator aoPressionar1()
-    {
-        yield return new WaitForSeconds(2);
-        GameObject XR = GameObject.FindGameObjectWithTag("Player");
-        ActiveAvatar activeAvatar = XR.GetComponent<ActiveAvatar>();
-        XR.transform.position = posicao1;
-        load1.SetActive(false);
-    }
 }
