064e214085ff5e41b31e53e0767b88937a21bc61
pedro_igor <pedrode13souza@gmail.com>
2024-01-30 13:02:09 -0300

    troca bola

--- Assets/Scripts/Tubo e Sequencia/CheckCorreto.cs
+++ Assets/Scripts/Tubo e Sequencia/CheckCorreto.cs
@@ -29,48 +29,52 @@ public class CheckCorreto : MonoBehaviourPunCallbacks
             Vector3 posicaoInicial = other.GetComponent<SphereFall>().posicaoInicial;
             if (numero == sequencia.getProximo())
             {
-
-                if (!other.GetComponent<XRGrabInteractable>().isSelected)
+                if (photonView.IsMine)
                 {
-                    //cria uma nova esfera no lugar inicial e atualiza a sequencia de bolas
-                    PhotonNetwork.InstantiateRoomObject(Path.Combine(pasta, "Sphere " + numero), posicaoInicial, Quaternion.identity);
-                    sequencia.tuboList.Add(other.gameObject);
-                    sequencia.updateProximo();
-
-                    //tira a possibilidade de segurar a bolinha inserida no tubo
-                    other.GetComponent<XRGrabInteractable>().enabled = false;
-
-                    //atualiza o quadro de pontos
-                    trocaPonto.TrocarPontos(10, true);
-
-                    //informa a LRS
-                    if (sequencia.concluido)
+                    if (!other.GetComponent<XRGrabInteractable>().isSelected)
                     {
-                        statementSender.SendStament("Bola", numero.ToString(), true, true);
+                        //cria uma nova esfera no lugar inicial e atualiza a sequencia de bolas
+                        PhotonNetwork.InstantiateRoomObject(Path.Combine(pasta, "Sphere " + numero), posicaoInicial, Quaternion.identity);
+                        sequencia.tuboList.Add(other.gameObject);
+                        sequencia.updateProximo();
+
+                        //tira a possibilidade de segurar a bolinha inserida no tubo
+                        other.GetComponent<XRGrabInteractable>().enabled = false;
+
+                        //atualiza o quadro de pontos
+                        trocaPonto.TrocarPontos(10, true);
+
+                        //informa a LRS
+                        if (sequencia.concluido)
+                        {
+                            statementSender.SendStament("Bola", numero.ToString(), true, true);
+                        }
+                        else
+                        {
+                            statementSender.SendStament("Bola", numero.ToString(), true, false);
+                        }
+
+
                     }
-                    else
-                    {
-                        statementSender.SendStament("Bola", numero.ToString(), true, false);
-                    }
-                    
 
                 }
+                else
+                {
+                    // ativa animação, destroi o objeto e cria uma nova esfera no lugar de origem
+                    this.photonView.RPC("PlaySmoke", RpcTarget.All);
+                    Destroy(other.gameObject);
+                    PhotonNetwork.InstantiateRoomObject(Path.Combine(pasta, "Sphere " + numero), posicaoInicial, Quaternion.identity);
 
-            }
-            else
-            {
-                // ativa animação, destroi o objeto e cria uma nova esfera no lugar de origem
-                this.photonView.RPC("PlaySmoke", RpcTarget.All);
-                Destroy(other.gameObject);
-                PhotonNetwork.InstantiateRoomObject(Path.Combine(pasta, "Sphere " + numero), posicaoInicial, Quaternion.identity);
+                    //atualiza o quadro de pontos
+                    trocaPonto.TrocarPontos(10, false);
 
-                //atualiza o quadro de pontos
-                trocaPonto.TrocarPontos(10, false);
-
-                //informa a LRS
-                statementSender.SendStament("Bola", numero.ToString(), false, false);
+                    //informa a LRS
+                    statementSender.SendStament("Bola", numero.ToString(), false, false);
+                }
             }
         }
+
+                
        
 
     }
