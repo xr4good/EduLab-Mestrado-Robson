e9e814b7011c4320bee31d176870c30149307cc6
pedro_igor <pedrode13souza@gmail.com>
2024-01-31 10:10:50 -0300

    Correct Setplayer

--- Assets/Scripts/LabScripts/SetPlayer.cs
+++ Assets/Scripts/LabScripts/SetPlayer.cs
@@ -23,34 +23,36 @@ public class SetPlayer : MonoBehaviour
         {
             gameDefinitions.ROOM = 1;
         }
-        else if (b == 3 || b == 4 || b == 19 || b == 20 || b == 35 || b == 36 || b == 51 || b == 52)
+        if (b == 17 || b == 18 || b == 21 || b == 22 || b == 25 || b == 26 || b == 29 || b == 30)
+        {
+            gameDefinitions.ROOM = 2;
+        }
+        else if (b == 33 || b == 34 || b == 37 || b == 38 || b == 41 || b == 42 || b == 45 || b == 46)
         {
             gameDefinitions.ROOM = 3;
         }
-        else if (b == 61 || b == 62 || b == 49 || b == 50 || b == 53 || b == 54 || b == 57 || b == 58)
+        else if (b == 49 || b == 50 || b == 53 || b == 54 || b == 57 || b == 58 || b == 61 || b == 62)
+        {
+            gameDefinitions.ROOM = 4;
+        }
+        else if (b == 3 || b == 4 || b == 19 || b == 20 || b == 35 || b == 36 || b == 51 || b == 52)
+        {
+            gameDefinitions.ROOM = 5;
+        }
+        else if (b == 7 || b == 8 || b == 23 || b == 24 || b == 39 || b == 40 || b == 55 || b == 56)
         {
             gameDefinitions.ROOM = 6;
         }
         else if (b == 11 || b == 12 || b == 27 || b == 28 || b == 43 || b == 44 || b == 59 || b == 60)
         {
-            gameDefinitions.ROOM = 4;
-        }
-        else if (b == 15 || b == 16 || b == 31 || b == 32 || b == 47 || b == 48 || b == 63 || b == 64)
-        {
-            gameDefinitions.ROOM = 8;
-        }
-        else if (b == 17 || b == 18 || b == 21 || b == 22 || b == 25 || b == 26 || b == 29 || b == 30)
-        {
-            gameDefinitions.ROOM = 5;
-        }
-        else if (b == 23 || b == 24 || b == 39 || b == 40 || b == 55 || b == 56 || b == 7 || b == 8)
-        {
             gameDefinitions.ROOM = 7;
         }
         else
         {
-            gameDefinitions.ROOM = 2;
+            gameDefinitions.ROOM = 8;
         }
+
+
 
 
         //gameDefinitions.ROOM = sliderRoom.value;
