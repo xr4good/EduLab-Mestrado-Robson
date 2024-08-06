9643a8b87718d4dd1b57f6dfbc130fd19a1ade28
rsvio <rsviol@hotmail.com>
2024-08-06 16:40:27 -0300

    incluído sistema de ajuda visual

---
+++ Assets/helperClick.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class helperClick : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public GameObject helperLeft;
+    public GameObject helperRight;
+
+    bool isActive = false;
+
+    public void switchActive()
+
+    {
+        if(isActive)
+        {
+            helperLeft.SetActive(false);
+            helperRight.SetActive(false);
+            isActive = false;
+        }
+        else
+        {
+            helperLeft.SetActive(true);
+            helperRight.SetActive(true);
+            isActive = true;
+        }
+        
+    }
+
+    
+}
